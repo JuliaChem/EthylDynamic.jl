ab566876a021bba572115c66ed334125fac28d00
Kelvyn Baruc Sánchez Sánchez <kelvyn.baruc@gmail.com>
2020-04-07 13:14:19 -0500

    Fix module 2

--- src/EthylDynamic.jl
+++ src/EthylDynamic.jl
@@ -1,21 +1,3 @@
-# Main module
-
-using Gtk, Gtk.ShortNames, JLD, Suppressor, CSV, Mustache, Dates
-import DataFrames
-
-# Path to CSS Gtk-Style dataFile
-global style_file = joinpath(dirname(Base.source_path()), "style2020.css")
-global img = Gtk.Image(joinpath(dirname(Base.source_path()), "media\\mainlogo.png"))
-
-# General Settings
-global pathDataBase = joinpath(dirname(Base.source_path()), "database.csv")
-
-# Load default database
-global database = CSV.read(pathDataBase)
-
-# Environmental variable to allow Windows decorations
-ENV["GTK_CSD"] = 0
-
 module EthylDynamic
   # Function to call the GUI
   export EthylDynamicGUI
